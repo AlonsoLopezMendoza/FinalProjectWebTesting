0d4454274001fc3df9fc589635f69da0b05e9ab5
unknown <alopez-mendoza@spartaglobal.com>
2020-04-24 10:26:40 +0100

    product Bys

--- src/test/java/com/sparta/team/pages/HomePage.java
+++ src/test/java/com/sparta/team/pages/HomePage.java
@@ -18,7 +18,70 @@ public class HomePage {
     By proceedToCheckoutButton = new By.ByCssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a > span");
     By proceedToCheckoutPopUpBox = new By.ByClassName("layer_cart_product col-xs-12 col-md-6");
 
+    By productImagePayment = new By.ByCssSelector("#product_1_1_0_304609 > td.cart_product > a > img");
+    By productNamePayment = new By.ByCssSelector("#product_1_1_0_304609 > td.cart_description > small.cart_ref");
+    By productSKUPayment = new By.ByCssSelector("#product_1_1_0_304609 > td.cart_description > small:nth-child(3) > a");
+    By productColorSizePayment = new By.ByCssSelector("#product_1_1_0_304609 > td.cart_description > small:nth-child(3) > a");
+    By productInStockPayment = new By.ByCssSelector("#product_1_1_0_304609 > td.cart_avail > span");
+    By productPricePayment = new By.ByCssSelector("#product_price_1_1_304609 > span");
+    By productQuantityPayment = new By.ByCssSelector("#product_1_1_0_304609 > td.cart_quantity.text-center > span");
+    By productTotalPayment = new By.ByCssSelector("#total_product_price_1_1_304609");
+    By totalProductsPayment = new By.ByCssSelector("<td colspan=\"2\" class=\"price\" id=\"total_product\">$66.04</td>");
+    By totalShippingPayment = new By.ByCssSelector("#total_shipping");
+    By totalPayment = new By.ByCssSelector("#total_price");
+
+    By deliveryOptionPriceShipping = new By.ByCssSelector("#form > div > div.delivery_options_address > div.delivery_options > div > div > table > tbody > tr > td.delivery_option_price > div");
+
     private final int TIMEOUTTIME = 5;
+
+    public String getProductImagePayment() {
+        return webDriver.findElement(productImagePayment).getAttribute("src");
+    }
+
+    public String getProductNamePayment() {
+        return webDriver.findElement(productNamePayment).getText();
+    }
+
+    public String getProductSKUPayment() {
+        return webDriver.findElement(productSKUPayment).getText();
+    }
+
+    public String getProductColorSizePayment() {
+        return webDriver.findElement(productColorSizePayment).getText();
+    }
+
+    public String getProductInStockPayment() {
+        return webDriver.findElement(productInStockPayment).getText();
+    }
+
+    public String getProductPricePayment() {
+        return webDriver.findElement(productPricePayment).getText();
+    }
+
+    public String getProductQuantityPayment() {
+        return webDriver.findElement(productQuantityPayment).getText();
+    }
+
+    public String getProductTotalPayment() {
+        return webDriver.findElement(productTotalPayment).getText();
+    }
+
+    public String getTotalProductsPayment() {
+        return webDriver.findElement(totalProductsPayment).getText();
+    }
+
+    public String getTotalShippingPayment() {
+        return webDriver.findElement(totalShippingPayment).getText();
+    }
+
+    public String getTotalPayment() {
+        return webDriver.findElement(totalPayment).getText();
+    }
+
+    public String getDeliveryOptionPriceShipping() {
+        return webDriver.findElement(deliveryOptionPriceShipping).getText();
+    }
+
 
     public HomePage(WebDriver webDriver){
         this.webDriver = webDriver;
